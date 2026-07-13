f49a20078d406aa832c22556b6c37d42424a3916
Andrzej <bazylek11@gmail.com>
2026-07-13 17:18:34 +0200

    feat: Patient service implemented

---
+++ src/main/java/com/andrzej_kosmowski/medical_clinic/service/PatientService.java
@@ -0,0 +1,49 @@
+package com.andrzej_kosmowski.medical_clinic.service;
+
+import com.andrzej_kosmowski.medical_clinic.exception.PatientAlreadyExistsException;
+import com.andrzej_kosmowski.medical_clinic.exception.PatientNotFoundException;
+import com.andrzej_kosmowski.medical_clinic.model.Patient;
+import com.andrzej_kosmowski.medical_clinic.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Patient getPatientByEmail(String email) {
+        return patientRepository.findByEmail(email)
+                .orElseThrow(() -> new PatientNotFoundException(email));
+    }
+
+    public Patient addPatient(Patient patient) {
+        if (patientRepository.existsByEmail(patient.getEmail())) {
+            throw new PatientAlreadyExistsException(patient.getEmail());
+        }
+        return patientRepository.save(patient);
+    }
+
+    public void deletePatientByEmail(String email) {
+        Patient patient = getPatientByEmail(email);
+
+        patientRepository.delete(patient);
+    }
+
+    public Patient updatePatientByEmail(String email, Patient updatedPatient) {
+        Patient existing = getPatientByEmail(email);
+
+        existing.setFirstName(updatedPatient.getFirstName());
+        existing.setLastName(updatedPatient.getLastName());
+        existing.setPhoneNumber(updatedPatient.getPhoneNumber());
+        existing.setBirthDate(updatedPatient.getBirthDate());
+
+        return patientRepository.save(existing);
+    }
+}
