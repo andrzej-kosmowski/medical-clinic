50d45c12cc2643e23d86bea4f7277b0474216dd3
Andrzej <bazylek11@gmail.com>
2026-07-13 17:19:12 +0200

    feat: Patient controller implemented

---
+++ src/main/java/com/andrzej_kosmowski/medical_clinic/controller/PatientController.java
@@ -0,0 +1,44 @@
+package com.andrzej_kosmowski.medical_clinic.controller;
+
+import com.andrzej_kosmowski.medical_clinic.model.Patient;
+import com.andrzej_kosmowski.medical_clinic.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+public class PatientController {
+    private final PatientService patientService;
+
+    @GetMapping
+    public List<Patient> getAll() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping("/{email}")
+    public Patient getPatient(@PathVariable String email) {
+        return patientService.getPatientByEmail(email);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Patient addPatient(@RequestBody Patient patient) {
+        return patientService.addPatient(patient);
+    }
+
+    @DeleteMapping("/{email}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePatientByEmail(@PathVariable String email) {
+        patientService.deletePatientByEmail(email);
+    }
+
+    @PatchMapping("/{email}")
+    @ResponseStatus(HttpStatus.OK)
+    public Patient updatePatientByEmail(@PathVariable String email, @RequestBody Patient patient) {
+        return patientService.updatePatientByEmail(email, patient);
+    }
+}
