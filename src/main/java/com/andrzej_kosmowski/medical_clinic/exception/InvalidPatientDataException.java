ddbae603da2b04cf8cf12d2dc69f91c94658fb04
Andrzej <bazylek11@gmail.com>
2026-07-20 12:42:05 +0200

    fix: get rid of unsued imports

--- src/main/java/com/andrzej_kosmowski/medical_clinic/exception/InvalidPatientDataException.java
+++ src/main/java/com/andrzej_kosmowski/medical_clinic/exception/InvalidPatientDataException.java
@@ -1,7 +1,6 @@
 package com.andrzej_kosmowski.medical_clinic.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class InvalidPatientDataException extends MedicalClinicException {
     public InvalidPatientDataException(String message) {
--- src/main/java/com/andrzej_kosmowski/medical_clinic/exception/PatientNotFoundException.java
+++ src/main/java/com/andrzej_kosmowski/medical_clinic/exception/PatientNotFoundException.java
@@ -1,7 +1,6 @@
 package com.andrzej_kosmowski.medical_clinic.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class PatientNotFoundException extends MedicalClinicException {
     public PatientNotFoundException(String email) {
