937666633766a82c4cd8ac0c9e03630b872c8bdf
Andrzej <bazylek11@gmail.com>
2026-08-03 17:19:50 +0200

    feat: Visit repository implemented

---
+++ src/main/java/com/andrzej_kosmowski/medical_clinic/repository/VisitRepository.java
@@ -0,0 +1,20 @@
+package com.andrzej_kosmowski.medical_clinic.repository;
+
+import com.andrzej_kosmowski.medical_clinic.model.Doctor;
+import com.andrzej_kosmowski.medical_clinic.model.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+    List<Visit> findAllByDoctorId(Long doctorId);
+    List<Visit> findAllByPatientId(Long patientId);
+    List<Visit> findAllByPatientIsNull();
+    boolean existsByDoctorIdAndStartTimeLessThanAndEndTimeGreaterThan(
+            Long id, LocalDateTime end, LocalDateTime start
+    );
+    boolean existsByPatientIdAndStartTimeLessThanAndEndTimeGreaterThan(
+            Long id, LocalDateTime end, LocalDateTime start
+    );
+}
