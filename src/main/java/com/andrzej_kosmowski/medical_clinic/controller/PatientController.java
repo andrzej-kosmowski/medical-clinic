4a2507cf744c0d24817005ce9b56db9a19d97f6c
Andrzej <bazylek11@gmail.com>
2026-08-06 21:20:12 +0200

    fix: get rid of unused imports

--- src/main/java/com/andrzej_kosmowski/medical_clinic/controller/PatientController.java
+++ src/main/java/com/andrzej_kosmowski/medical_clinic/controller/PatientController.java
@@ -18,8 +18,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(value = "/patients", produces = "application/json")
 @RequiredArgsConstructor
