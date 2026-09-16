1e85a9ec16cbaf79361ac213373058e075c96bc1
Andrzej <bazylek11@gmail.com>
2026-09-16 17:11:08 +0200

    fix: get rid of unused import

--- src/test/java/com/andrzej_kosmowski/medical_clinic/controller/DoctorControllerTest.java
+++ src/test/java/com/andrzej_kosmowski/medical_clinic/controller/DoctorControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
